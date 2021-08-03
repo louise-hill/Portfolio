b31822265c80249a7a0dd542797efd85f3535481
louise hill <louisehilldesigner@gmail.com>
2021-08-03 15:21:50 +0100

    seo and custom 404s

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   get 'pages/home'
   get 'pages/cultivate'
   get 'pages/inky'
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
   get 'pages/smb'
   get 'pages/about'
   root to: 'pages#home'
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
