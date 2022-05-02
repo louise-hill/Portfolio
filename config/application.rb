8e0c83ba1c60a06075ae2aa45ae8fc990c32bf38
louise hill <louisehilldesigner@gmail.com>
2022-05-03 00:00:36 +0100

    trial

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Portfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.exceptions_app = self.routes
-    config.assets.paths << Rails.root.join("app","assets","fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
