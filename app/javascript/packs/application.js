58ac59c9cf4dc83d08734bf9150eda8e514eb0a8
louise hill <louisehilldesigner@gmail.com>
2022-05-02 20:39:05 +0100

    lol

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,5 +17,4 @@ require("bootstrap")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "controllers";
 import "bootstrap";
